1518e2e725bf4b6877d28e794d2349b7c6eb40f6
Kazeem Lawal <quazeemastic@gmail.com>
2024-05-31 16:00:26 +0100

    Chapter1 - continuation

--- server.js
+++ server.js
@@ -10,5 +10,24 @@
 console.log('Hello World')
 
 //3) global object instead of window object
+
 console.log(global);
+
 //4) Has Common core modules that we will explore
+//5) CommonJS modules instead of ES6 modules
+
+const os = require('os');
+const path = require('path');
+
+console.log(os.type());
+console.log(os.version());
+console.log(os.homedir());
+
+console.log(__dirname);
+console.log(__filename);
+
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
