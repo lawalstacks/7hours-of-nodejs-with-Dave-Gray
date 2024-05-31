966f4270bcde59b5c76da432f8b9cc263b70be45
Kazeem Lawal <quazeemastic@gmail.com>
2024-05-31 15:50:54 +0100

    Chapter1 - How node js differs from vanilla js

---
+++ server.js
@@ -0,0 +1,14 @@
+//you should already know:
+//HTML,CSS,javascript
+//Possibly exprerience with other libraries and frameworks
+
+
+//How NodeJs differs from vanilla JS
+//1) Node runs on a server - not in a browser (backend not frontend)
+//2) The console is the terminal window
+
+console.log('Hello World')
+
+//3) global object instead of window object
+console.log(global);
+//4) Has Common core modules that we will explore
