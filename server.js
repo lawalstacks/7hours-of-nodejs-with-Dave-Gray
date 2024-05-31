c92fc038db16dd90d44d8975eb82964c4493d13a
Kazeem Lawal <quazeemastic@gmail.com>
2024-05-31 16:11:17 +0100

    Chapter1 - custom modules(END)

--- server.js
+++ server.js
@@ -11,15 +11,22 @@ console.log('Hello World')
 
 //3) global object instead of window object
 
-console.log(global);
+//console.log(global);
 
 //4) Has Common core modules that we will explore
 //5) CommonJS modules instead of ES6 modules
+//6) missing some js API like fetch
 
 const os = require('os');
 const path = require('path');
+const {add,subtract,multiply,divide} = require('./math');
 
-console.log(os.type());
+console.log(add(2,3));
+console.log(subtract(2,3));
+console.log(multiply(2,3));
+console.log(divide(2,3));
+
+/*console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
 
@@ -31,3 +38,4 @@ console.log(path.basename(__filename));
 console.log(path.extname(__filename));
 
 console.log(path.parse(__filename));
+*/
